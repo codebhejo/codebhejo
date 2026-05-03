8bafd62ee99b38b747d5dc6bdb6e27a708502891
Govind yadav <govindsvyadav@gmail.com>
2026-05-03 17:18:51 +0530

    backend image build test

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -12,18 +12,17 @@ const app = express();
 
 app.use(morgan("dev"));
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://codebhejo.in"
-  ],
-  methods: ["GET", "POST", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://codebhejo.in"],
+    methods: ["GET", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 
 app.use("/code", codeRoutes);
@@ -32,6 +31,5 @@ app.use("/p2p", p2pRoutes);
 app.use("/admin", adminRoutes);
 
 app.use(errorHandler);
-
 
 export default app;
